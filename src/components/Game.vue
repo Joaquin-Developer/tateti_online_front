e329ce9c618ef985bc0222877e3f23137cb8e35d
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-04-27 23:36:29 -0300

    estructura básica de proyecto vue

---
+++ src/components/Game.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <h1>Inicio...</h1>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+    title: "Inicio",
+})
+</script>
+
+<style>
+
+</style>
