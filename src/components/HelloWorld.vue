633e879f02dfc77358a2b5e50a5c2b52d7b37a2a
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-05-02 13:48:38 -0300

    actualizo estructura de proyecto vuejs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,33 @@
 <template>
   <div>
-    <p>Hola</p>
+    <p>Holis</p>
   </div>
+  <form>
+    <input v-model="myName" class="input" type="text">
+    <button class="btnSend" v-on:click="showName">Enviar</button>
+    <p style="color: #000;">{{ myName }}</p>
+  </form>
 
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  myName: null,
   props: {
     msg: String
+  },
+  methods: {
+    showName: function(evt) {
+      evt.preventDefault();
+      alert("Hola");
+    }
   }
 }
 </script>
 
-<style></style>
+<style>
+.btnSend, .input {
+  color: #000;
+}
+</style>
